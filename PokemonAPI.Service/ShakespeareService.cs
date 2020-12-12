17d5989b6d623ed22267762829ff29758e219994
Florusso <f.lorusso@gamenetgroup.it>
2020-12-12 19:07:34 +0100

    Implemented and Tested Shakespeare API

--- PokemonAPI.Service/ShakespeareService.cs
+++ PokemonAPI.Service/ShakespeareService.cs
@@ -1,15 +1,58 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using PokemonAPI.Model;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PokemonAPI.Service
 {
     public class ShakespeareService
     {
-        public string Translate(string text)
+        public async Task<string> TranslateAsync(string text)
         {
+            var shakespearResource = $"https://api.funtranslations.com/translate/shakespeare.json?text={text}";
 
-            return "";
+            var translated = string.Empty;
+            try
+            {                                         
+
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage response = await client.GetAsync(shakespearResource))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            using (HttpContent content = response.Content)
+                            {
+                                string myContent = await content.ReadAsStringAsync();
+
+                                var ret = JsonConvert.DeserializeObject<ShakespeareApiResponse>(myContent);
+
+                                if (ret.success.total > 0)
+                                {
+                                    translated = ret.contents.translated;
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            //response.ReasonPhrase
+                           // response.StatusCode
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+               // _logger.LogError(ex, "ShakespeareService.Tranlaste");
+            }
+
+            
+            return translated;
         }
     }
 }
---
+++ PokemonAPI.ServiceTests/ShakespeareServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokemonAPI.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonAPI.Service.Tests
+{
+    [TestClass()]
+    public class ShakespeareServiceTests
+    {
+        [TestMethod()]
+        public void TranslateAsyncTest()
+        {
+            var text = "Ups FIRE moves in a pinch.";//"You gave Mr. Tim a hearty meal, but unfortunately what he ate made him die.";
+            var svc = new ShakespeareService();
+            var translated=  svc.TranslateAsync(text).GetAwaiter().GetResult();
+
+            Assert.IsNotNull(translated);
+        }
+    }
+}
