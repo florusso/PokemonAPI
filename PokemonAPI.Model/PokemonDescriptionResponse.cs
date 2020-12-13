d07e2ace25177388ea2735b5643f37b2064d5548
Florusso <f.lorusso@gamenetgroup.it>
2020-12-13 11:13:50 +0100

    WIP WebApi

---
+++ PokemonAPI.Model/PokemonDescriptionResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace PokemonAPI.Model
+{
+   public class PokemonDescriptionResponse
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("descriprion")]
+        public string Description { get; set; }
+    }
+}
--- PokemonAPI/Controllers/PokemonController.cs
+++ PokemonAPI/Controllers/PokemonController.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using PokemonAPI.Model;
 
 namespace PokemonAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class PokemonController : ControllerBase
     {
@@ -17,6 +18,13 @@ namespace PokemonAPI.Controllers
         public PokemonController(ILogger<PokemonController> logger)
         {
             _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/pokemon/{pokemon_name}")]
+        public PokemonDescriptionResponse GetPokemonDescription(string pokemon_name)
+        {
+            return new PokemonDescriptionResponse { Name = pokemon_name, Description = "Super" };
         }
     }
 }
