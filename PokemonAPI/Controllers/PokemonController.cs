444bbb09d55d2f5b0dfe92e98c8ebc2621380836
Florusso <f.lorusso@gamenetgroup.it>
2020-12-13 12:37:55 +0100

    Update PokemonController.cs

--- PokemonAPI/Controllers/PokemonController.cs
+++ PokemonAPI/Controllers/PokemonController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PokemonAPI.Model;
+using PokemonAPI.Service;
 
 namespace PokemonAPI.Controllers
 {
@@ -14,17 +15,55 @@ namespace PokemonAPI.Controllers
     public class PokemonController : ControllerBase
     {
         private readonly ILogger<PokemonController> _logger;
+        private IPokemonService _pokemonService;
+        private IShakespeareService _shakespeareService;
 
-        public PokemonController(ILogger<PokemonController> logger)
+        public PokemonController(ILogger<PokemonController> logger, IPokemonService pokemonService, IShakespeareService shakespeareService)
         {
             _logger = logger;
+            _pokemonService = pokemonService;
+            _shakespeareService = shakespeareService;
+
         }
 
         [HttpGet]
         [Route("/pokemon/{pokemon_name}")]
-        public PokemonDescriptionResponse GetPokemonDescription(string pokemon_name)
+        public async Task<ActionResult<PokemonDescriptionResponse>> GetPokemonDescriptionAsync(string pokemon_name)
         {
-            return new PokemonDescriptionResponse { Name = pokemon_name, Description = "Super" };
+            var ret = new PokemonDescriptionResponse();
+            ret.Name = pokemon_name;
+            try
+            {
+                var desc = await _pokemonService.FindDescriptionByNameAsync(pokemon_name);
+
+                if (desc is null) return InternalError();
+               
+
+                if (desc == string.Empty)
+                {
+                    _logger.LogWarning($"{pokemon_name}: not found");
+                    return NotFound();
+                }
+
+
+                ret.Description = await _shakespeareService.TranslateAsync(desc);
+
+                if (string.IsNullOrEmpty(ret.Description)) return InternalError();
+              
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+
+                throw;
+            }
+            return Ok(ret);
+        }
+
+        private ObjectResult InternalError()
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Something went really wrong, we are very sorry!");
         }
     }
 }
